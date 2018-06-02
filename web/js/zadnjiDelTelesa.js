fd7d4bd31e6c7730089fe2f4ee511ac9497d4ca6
r1b1c <jozef.ribic@student.um.si>
2018-06-02 14:43:55 +0200

    urejanje in dodajanje dele telesa da ko kliknema na del telesa se odprejo vadbe

---
+++ web/js/zadnjiDelTelesa.js
@@ -0,0 +1,21 @@
+function rokeZadaj(e) {
+    document.getElementById("demo1").innerHTML = '<img src="images/zadaj_roke.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function nogeZadaj(e) {
+    document.getElementById("demo1").innerHTML = '<img src="images/zadaj_noge.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function hrbet(e) {
+    document.getElementById("demo1").innerHTML = '<img src="images/zadaj_hrbet.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function rit(e) {
+    document.getElementById("demo1").innerHTML = '<img src="images/zadaj_rit.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function pocistiZadaj() {
+    document.getElementById("demo1").innerHTML = '<img src="images/zadaj.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+
