70ba16c813c133130ad43606af62397e22936b48
r1b1c <jozef.ribic@student.um.si>
2018-06-04 16:01:16 +0200

    dodajanje ludeka

---
+++ web/js/zadnjiDelTelesa.js
@@ -0,0 +1,21 @@
+function rokeZadaj(e) {
+    document.getElementById("zadaj").innerHTML = '<img src="images/zadajRoke.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function nogeZadaj(e) {
+    document.getElementById("zadaj").innerHTML = '<img src="images/zadajNoge.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function hrbet(e) {
+    document.getElementById("zadaj").innerHTML = '<img src="images/zadajHrbet.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function rit(e) {
+    document.getElementById("zadaj").innerHTML = '<img src="images/zadajRit.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+function pocistiZadaj() {
+    document.getElementById("zadaj").innerHTML = '<img src="images/zadaj.png"  alt="teloZadaj" usemap="#teloZadaj">';
+}
+
+
