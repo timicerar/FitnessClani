f6b99e0e7d0e5a21eb98256627514d6e07d82ef4
timicerar <timotej.cerar@student.um.si>
2018-06-06 04:53:25 +0200

    Jquery, JS

--- web/js/sprednjiDelTelesa.js
+++ web/js/sprednjiDelTelesa.js
@@ -1,17 +1,17 @@
 function roke(e) {
-    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/spredajRoke.png"  alt="teloSpredaj" usemap="#teloSpredaj">';
+    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/SpredajRoke.png" title="Vadbe za roke" alt="teloSpredaj" usemap="#teloSpredaj">';
 }
 
 function noge(e) {
-    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/spredajNoge.png"  alt="teloSpredaj" usemap="#teloSpredaj">';
+    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/spredajNoge.png" title="Vadbe za noge" alt="teloSpredaj" usemap="#teloSpredaj">';
 }
 
 function prsa(e) {
-    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/spredajPrsa.png"  alt="teloSpredaj" usemap="#teloSpredaj">';
+    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/spredajPrsa.png" title="Vadbe za prsa" alt="teloSpredaj" usemap="#teloSpredaj">';
 }
 
 function trebuh(e) {
-    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/spredajTrebuh.png"  alt="teloSpredaj" usemap="#teloSpredaj">';
+    document.getElementById("demo").innerHTML = '<img src="fitness-body-img/spredajTrebuh.png" title="Vadbe za trebusne misice" alt="teloSpredaj" usemap="#teloSpredaj">';
 }
 
 function pocisti() {
--- web/js/zadnjiDelTelesa.js
+++ web/js/zadnjiDelTelesa.js
@@ -1,21 +1,21 @@
 function rokeZadaj(e) {
-    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajRoke.png"  alt="teloZadaj" usemap="#teloZadaj">';
+    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajRoke.png" title="Vadbe za roke" alt="teloZadaj" usemap="#teloZadaj">';
 }
 
 function nogeZadaj(e) {
-    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajNoge.png"  alt="teloZadaj" usemap="#teloZadaj">';
+    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajNoge.png" title="Vadbe za noge" alt="teloZadaj" usemap="#teloZadaj">';
 }
 
 function hrbet(e) {
-    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajHrbet.png"  alt="teloZadaj" usemap="#teloZadaj">';
+    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajHrbet.png" title="Vadbe za hrbet" alt="teloZadaj" usemap="#teloZadaj">';
 }
 
 function rit(e) {
-    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajRit.png"  alt="teloZadaj" usemap="#teloZadaj">';
+    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadajRit.png" title="Vadbe za zadnjico" alt="teloZadaj" usemap="#teloZadaj">';
 }
 
 function pocistiZadaj() {
-    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadaj.png"  alt="teloZadaj" usemap="#teloZadaj">';
+    document.getElementById("zadaj").innerHTML = '<img src="fitness-body-img/zadaj.png" alt="teloZadaj" usemap="#teloZadaj">';
 }
 
 
